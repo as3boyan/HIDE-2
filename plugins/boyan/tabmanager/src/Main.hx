013ea3b3200cb27d578cc5530f16bd1e5b6315da
as3boyan <denis.flash.as3@gmail.com>
2013-11-18 21:17:38 +0200

    boyan.tabmanager plugin

---
+++ plugins/boyan/tabmanager/src/Main.hx
@@ -0,0 +1,32 @@
+package ;
+import js.Browser;
+import js.html.Element;
+import js.html.UListElement;
+
+/**
+ * ...
+ * @author AS3Boyan
+ */
+class Main
+{
+	public static var name:String = "boyan.tabmanager";
+	public static var dependencies:Array<String> = ["boyan.split-pane"];
+	
+	//If this plugin is selected as active in HIDE, then HIDE will call this function once on load	
+	public static function main():Void
+	{	
+		//Load CSS for tab styling
+		HIDE.loadCSS("../plugins/boyan/tabmanager/bin/includes/css/tabs.css");
+		
+		//Will wait for dependent plugins
+		HIDE.waitForDependentPluginsToBeLoaded(dependencies, load);
+	}
+	
+	private static function load():Void
+	{
+		TabManager.init();
+		
+		//Notify HIDE that plugin is ready for use, so plugins that depend on this plugin can start load themselves		
+		HIDE.plugins.push(name);
+	}
+}
---
+++ plugins/boyan/tabmanager/src/TabManager.hx
@@ -0,0 +1,92 @@
+package ;
+import js.Browser;
+import js.html.Element;
+import js.html.LIElement;
+import js.html.SpanElement;
+import js.html.UListElement;
+
+/**
+ * ...
+ * @author AS3Boyan
+ */
+class TabManager
+{
+	private static var tabs:UListElement;
+	
+	public static function init():Void
+	{
+		//<ul class="tabs" id="docs" style="-webkit-touch-callout: none; -webkit-user-select: none; user-select: none;"></ul>
+		
+		tabs = Browser.document.createUListElement();
+		tabs.className = "tabs";
+		
+		tabs.onclick = function(e):Void
+		{
+			var target:Dynamic = e.target || e.srcElement;
+			if (target.nodeName.toLowerCase() != "li") return;
+
+			var i = 0;
+			var c:Dynamic = target.parentNode.firstChild;
+
+			if (c == target)
+			{
+				return TabManager.selectDoc(0);
+			}
+			else
+			{
+				while (true)
+				{
+					i++;
+					c = c.nextSibling;
+					if (c == target) return TabManager.selectDoc(i);
+				}
+			}                        
+		};
+		
+		createNewTab("test");
+		
+		Splitpane.components[1].appendChild(tabs);
+	}
+	
+	public static function createNewTab(name:String):Void
+	{
+		var li:LIElement = Browser.document.createLIElement();
+		//li.title = path;
+		li.innerText = name + "\t";
+		//li.setAttribute("path", path);
+
+		var span:SpanElement = Browser.document.createSpanElement();
+		span.style.position = "relative";
+		span.style.top = "2px";
+
+		span.onclick = function (e):Void
+		{
+			//closeTab(path);
+		}
+
+		var span2:SpanElement = Browser.document.createSpanElement();
+		span2.className = "glyphicon glyphicon-remove-circle";
+		span.appendChild(span2);
+
+		li.appendChild(span);
+
+		tabs.appendChild(li);
+	}
+	
+	public static function selectDoc(pos:Int):Void
+	{
+		for (i in 0...tabs.childNodes.length)
+		{
+			var child:Element = cast(tabs.childNodes[i], Element);
+          
+			if (pos == i)
+			{
+				child.className = "selected";
+			}
+			else
+			{
+				child.className = "";
+			}
+		}
+	}
+}
