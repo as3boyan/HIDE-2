2218102b9e19ea2f01bae96bd65f5ec42afc05c4
as3boyan <denis.flash.as3@gmail.com>
2014-02-23 22:50:54 +0200

    bug fix for run.n script(for Windows)

--- src/Run.hx
+++ src/Run.hx
@@ -12,7 +12,20 @@ class Run
 {
 	public static function main()
 	{
-		var output = new sys.io.Process("haxelib", ["path", "HIDE"]).stdout.readAll().toString();
-		Sys.command("haxelib", ["run", "node-webkit", output.split("\n")[0] + "/bin" ]);
+		var process = new sys.io.Process("haxelib", ["path", "HIDE"]);
+		var output = process.stdout.readAll().toString();		
+		var path:String = output.split("\n")[0];
+
+		Sys.command("haxelib", ["run", "node-webkit", combine(StringTools.trim(path), "bin")]);
+	}
+	
+	public static function combine(firstPath:String, secondPath:String):String
+	{
+		if (!StringTools.endsWith(firstPath, "\\") && !StringTools.endsWith(firstPath, "/"))
+		{
+			firstPath += "/";
+		}
+		
+		return firstPath + secondPath;
 	}
 }
